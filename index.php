5f15441f68b7a143fd823f985950e4ecb9051fc9
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-06-11 23:58:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <pre>
-WELCOME TO MY PAGE CLOUD PAGE
-<br/>
 <html>
-   <head>
-      <title>HTML Backgorund Color</title>
-   </head>
+<head>
+<h1>WELCOME TO MY PAGE CLOUD PAGE</h1>
+<br/>
+<title>SHARVARY</title>
+</head>
 <?php
 print `ifconfig`;
 print `cal`;
 print `date`;
  ?>
 <body style="background-color:aqua;">
-<h1>Products</h1>
+<h2>MESSAGE by SSS(S^3)</h2>
+<p>FUCKK life </p>
+
 <p>developed by soul_leo.</p>
 </body>
 </html>
