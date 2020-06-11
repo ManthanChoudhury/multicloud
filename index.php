a342577796eaa58b342698b87c50b6e4f72e2135
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-06-11 23:31:05 +0530

    Update index.php
    qwe

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 WELCOME TO MY PAGE CLOUD PAGE
 <br/>
-
-<pre>
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Backgorund Color</title>
+   </head>
+ <pre>
 <?php
 print `ifconfig`;
 print `cal`;
 print `date`;
  ?>
  </pre>
+   <body style="background-color:aqua;">
+      <h1>Products</h1>
+      <p>developed by soul_leo.</p>
+   </body>
+</html>
+
