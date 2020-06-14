b46674f7171fa01e58337ca0b38dbcc308bc5524
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-06-14 23:56:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ print `cal`;
 print `date`;
  ?>
 <body style="background-color:aqua;">
-<img src="https://a8019225772.s3.ap-south-1.amazonaws.com/spiderman.jpg" alt="spiderman300" width="500" height="600">
-<h2>SPIDER MAN</h2>
+<img src="https://a8019225772.s3.ap-south-1.amazonaws.com/automatiiion.jpg" alt="automatiiion300" width="500" height="600">
+<h2>THE POWER OF AUTOMATION</h2>
 
 <p>developed by soul_leo.</p>
 </body>
